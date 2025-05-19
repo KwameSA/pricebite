8affb2ca2f0c59a5ec17b91bdf46bcf2d1c4ffe1
Samuel Akuffo <131817095+KwameSA@users.noreply.github.com>
2025-05-18 20:00:33 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-import { setupLoginForm } from "auth.js";
-import { setupMenu } from "menu.js";
-import { setupBudgetTrackers } from "budget.js";
-import { setupFilters } from "filters.js";
-import { setupToggleAlerts } from "alerts.js";
-import { setupBuyNow } from "buyNow.js";
-import { setupMonitoring } from "monitoring.js";
-import { setupWishlistItems } from "wishlist.js";
-import { setupImages } from "productImages.js";
-import { setupSearch } from "search.js";
-import { setupOptions } from "selectOptions.js";
+import { setupLoginForm } from "./auth.js";
+import { setupMenu } from "./menu.js";
+import { setupBudgetTrackers } from "./budget.js";
+import { setupFilters } from "./filters.js";
+import { setupToggleAlerts } from "./alerts.js";
+import { setupBuyNow } from "./buyNow.js";
+import { setupMonitoring } from "./monitoring.js";
+import { setupWishlistItems } from "./wishlist.js";
+import { setupImages } from "./productImages.js";
+import { setupSearch } from "./search.js";
+import { setupOptions } from "./selectOptions.js";
 
 function initializeApp() {
   setupMenu();
